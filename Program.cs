5855ba18e4eebd93699efdebe41ff72ebc865635
afaq imanova <afaq@imanov.name>
2026-02-02 12:44:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassesProgram.Math;
+
+namespace ClassesProgram
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Doe";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result=calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
